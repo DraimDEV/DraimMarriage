2ee48bfc6038a8f38e451aab9fa8148cf48ff8d3
DraimCiDo <danilaorlov4@gmail.com>
2022-02-01 21:09:05 +0200

    [1.0.0] - Создание репозитория

---
+++ src/main/java/ru/draimdev/draimmarriage/DraimMarriage.java
@@ -0,0 +1,17 @@
+package ru.draimdev.draimmarriage;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DraimMarriage extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
