5609e8a4ae693a2d6b303cb9bb3692ac07c4f897
DraimCiDo <danilaorlov4@gmail.com>
2022-02-04 11:25:00 +0200

    [1.0.13]

--- src/main/java/ru/draimdev/draimmarriage/Utils/ColorUtils.java
+++ src/main/java/ru/draimdev/draimmarriage/Utils/ColorUtils.java
@@ -1,4 +1,24 @@
 package ru.draimdev.draimmarriage.Utils;
 
+import org.bukkit.ChatColor;
+
 public class ColorUtils {
+
+    private static final String RAW_GRADIENT_HEX_REGEX = "<\\$#[A-Fa-f0-9]{6}>";
+    public static final String HEX_REGEX = "#[A-Fa-f0-9]{6}";
+    public static final String TAG_REGEX = "^[A-Za-z0-9_]{1,16}$";
+    public static final String RAW_HEX_REGEX = "<#[A-Fa-f0-9]{6}>";
+
+    public static String colorMessage(String legacyMSG) {
+        try{
+            if(legacyMSG.isEmpty()){
+                return legacyMSG;
+            }
+            if (ChatColor.translateAlternateColorCodes('&', legacyMSG).isEmpty()){
+                return ChatColor.translateAlternateColorCodes('&', legacyMSG);
+            }
+        } catch (NullPointerException e){
+            return legacyMSG;
+        }
+    }
 }
