2235e5e6812f36cfa197c681a48739ebc6e7fefe
DraimCiDo <danilaorlov4@gmail.com>
2022-02-04 11:21:04 +0200

    [1.0.12] - Добавление звуков при отпуте команд

--- src/main/java/ru/draimdev/draimmarriage/Utils/SoundUtils.java
+++ src/main/java/ru/draimdev/draimmarriage/Utils/SoundUtils.java
@@ -1,4 +1,11 @@
 package ru.draimdev.draimmarriage.Utils;
 
+import org.bukkit.Sound;
+import org.bukkit.entity.Player;
+
 public class SoundUtils {
+
+    public static void playSound(Player p, Sound sound) {
+        p.playSound(p.getLocation(), sound, 1.0F, 1.0F);
+    }
 }
