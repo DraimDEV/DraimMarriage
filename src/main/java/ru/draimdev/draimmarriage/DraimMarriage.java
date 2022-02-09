f5f3ad79d5d4094bc56e67b5805c44d473b38e81
DraimCiDo <danilaorlov4@gmail.com>
2022-02-09 23:59:05 +0200

    [1.1.0] Подумал чёт я, и послал нахуй это говно, потому что сделано на коленках, ре-код с нуля

--- src/main/java/ru/draimdev/draimmarriage/DraimMarriage.java
+++ src/main/java/ru/draimdev/draimmarriage/DraimMarriage.java
@@ -1,47 +1,20 @@
 package ru.draimdev.draimmarriage;
 
-import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
-import ru.draimdev.draimmarriage.Commands.MainCommands;
-import ru.draimdev.draimmarriage.Commands.TabComplete;
-import ru.draimdev.draimmarriage.Config.MessageConfig;
 
-import java.util.List;
-
-public final class DraimMarriage extends JavaPlugin
-{
-    public static FileConfiguration config;
-    private static DraimMarriage instance;
-
-    // Подхват всех настроек из config.yml
-    public static String getConfigString(String path) {
-        return config.getString(path);
-    }
-    public static List<String> getConfigStringList(String path) {
-        return config.getStringList(path);
-    }
-
-    public static DraimMarriage getInstance() {
-        return instance;
-    }
+public final class DraimMarriage extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        instance = this;
-        initCMDs();
-        this.saveDefaultConfig();
-        config = getConfig();
-        MessageConfig.getMSG().setUp();
+        /*
+
+         */
     }
 
     @Override
     public void onDisable() {
-        this.getLogger().info("Плагин успешно отключен.");
+        /*
 
-    }
-
-    public void initCMDs() {
-        getCommand("draimmarriage").setExecutor(new MainCommands(this));
-        getCommand("draimmarriage").setTabCompleter(new TabComplete());
+        */
     }
 }
