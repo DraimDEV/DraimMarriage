be7519f8148e749f255ccf7baff87f5449bc2502
DraimCiDo <danilaorlov4@gmail.com>
2022-02-10 00:05:13 +0200

    [1.1.1] Добавление подхвата getLogger()

--- src/main/java/ru/draimdev/draimmarriage/DraimMarriage.java
+++ src/main/java/ru/draimdev/draimmarriage/DraimMarriage.java
@@ -2,13 +2,15 @@ package ru.draimdev.draimmarriage;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.logging.Logger;
+
 public final class DraimMarriage extends JavaPlugin {
+
+    private static Logger log;
 
     @Override
     public void onEnable() {
-        /*
-
-         */
+        log = getLogger();
     }
 
     @Override
