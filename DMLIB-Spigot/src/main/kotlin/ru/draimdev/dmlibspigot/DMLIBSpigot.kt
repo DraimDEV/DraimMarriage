9d95a405849eb305b4b641e13db0c44e9216ade7
DraimCiDo <danilaorlov4@gmail.com>
2022-02-10 14:28:10 +0200

    [1.1.22]

--- DMLIB-Spigot/src/main/kotlin/ru/draimdev/dmlibspigot/DMLIBSpigot.kt
+++ DMLIB-Spigot/src/main/kotlin/ru/draimdev/dmlibspigot/DMLIBSpigot.kt
@@ -1,12 +1,16 @@
 package ru.draimdev.dmlibspigot
 
 import org.bukkit.plugin.java.JavaPlugin
+import ru.draimdev.dmlibspigot.DataBase.DataSource
+import net.kyori.adventure.platform.bukkit.BukkitAudiences
 
 class DMLIBSpigot : JavaPlugin() {
+    companion object {
+        lateinit var instance: DMLIBSpigot
+        lateinit var dataSource: DataSource
+        lateinit var bukkitAudiences: BukkitAudiences
+    }
 
     override fun onEnable() {
-        /*
-
-          */
     }
 }
