ad73b191c71389e47ac4b4d91d68eca84b92a00a
DraimCiDo <danilaorlov4@gmail.com>
2022-02-10 14:19:03 +0200

    [1.1.18]

--- DMLIB-Spigot/src/main/kotlin/ru/draimdev/dmlibspigot/Title/TitleUtils.kt
+++ DMLIB-Spigot/src/main/kotlin/ru/draimdev/dmlibspigot/Title/TitleUtils.kt
@@ -1,4 +1,12 @@
 package ru.draimdev.dmlibspigot.Title
 
-class TitleUtils {
+import org.bukkit.Bukkit
+import org.bukkit.entity.Player
+import ru.draimdev.dmlibspigot.DMLIBSpigot
+
+private val BUKKIT_AUDIENCES = DMLIBSpigot.bukkitAudiences
+fun sendTitle(player: Player, title: DMTitle) = BUKKIT_AUDIENCES.player(player).showTitle(title.toAdventureTitle())
+fun broadcastTitle(title: DMTitle) = Bukkit.getOnlinePlayers().forEach { sendTitle(it, title) }
+fun clearTitle(player: Player) {
+    BUKKIT_AUDIENCES.player(player).clearTitle()
 }
