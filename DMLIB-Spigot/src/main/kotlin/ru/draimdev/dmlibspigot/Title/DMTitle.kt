4da996f2d00d3bdc6105cb0c2559c4ff0056a40b
DraimCiDo <danilaorlov4@gmail.com>
2022-02-10 14:21:08 +0200

    [1.1.20]

--- DMLIB-Spigot/src/main/kotlin/ru/draimdev/dmlibspigot/Title/DMTitle.kt
+++ DMLIB-Spigot/src/main/kotlin/ru/draimdev/dmlibspigot/Title/DMTitle.kt
@@ -1,4 +1,18 @@
 package ru.draimdev.dmlibspigot.Title
 
-class DMTitle {
+import net.kyori.adventure.text.Component
+import net.kyori.adventure.title.Title
+import net.kyori.adventure.util.Ticks
+import org.bukkit.entity.Player
+
+class DMTitle(val title: Component, val subtitle: Component, val inTime: Long, val stayTime: Long, val outTime: Long) {
+
+    fun send(player: Player) = sendTitle(player, this)
+
+    fun broadcast() = broadcastTitle(this)
+
+    fun toAdventureTitle(): Title {
+        return Title.title(title, subtitle,
+            Title.Times.of(Ticks.duration(inTime), Ticks.duration(stayTime), Ticks.duration(outTime)))
+    }
 }
