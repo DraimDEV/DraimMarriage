c517ecde3bbf6aa911c8b2887096202eed974105
DraimCiDo <danilaorlov4@gmail.com>
2022-02-10 14:52:42 +0200

    [1.1.35]

--- DMLIB-Spigot/src/main/kotlin/ru/draimdev/dmlibspigot/DMLIBSpigot.kt
+++ DMLIB-Spigot/src/main/kotlin/ru/draimdev/dmlibspigot/DMLIBSpigot.kt
@@ -3,12 +3,17 @@ package ru.draimdev.dmlibspigot
 import org.bukkit.plugin.java.JavaPlugin
 import ru.draimdev.dmlibspigot.DataBase.DataSource
 import net.kyori.adventure.platform.bukkit.BukkitAudiences
+import net.milkbowl.vault.economy.Economy
+import ru.draimdev.dmlibspigot.Player.PlayerQuery
 
 class DMLIBSpigot : JavaPlugin() {
     companion object {
         lateinit var instance: DMLIBSpigot
         lateinit var dataSource: DataSource
         lateinit var bukkitAudiences: BukkitAudiences
+        var vaultEcon: Economy? = null
+        lateinit var playerQuery: PlayerQuery
+
     }
 
     override fun onEnable() {
