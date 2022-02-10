97b8bfb1b50ac6d134e665ea8ab058e0e8d3695a
DraimCiDo <danilaorlov4@gmail.com>
2022-02-10 14:42:43 +0200

    [1.1.28]

--- DMLIB-Spigot/src/main/kotlin/ru/draimdev/dmlibspigot/ActionBar/ActionBarBuilder.kt
+++ DMLIB-Spigot/src/main/kotlin/ru/draimdev/dmlibspigot/ActionBar/ActionBarBuilder.kt
@@ -1,4 +1,29 @@
 package ru.draimdev.dmlibspigot.ActionBar
 
-class ActionBarBuilder {
+import ru.draimdev.dmlibspigot.Util.colorString
+
+class ActionBarBuilder(var message: String, var duration: Long) {
+
+    constructor() : this("", 20)
+
+    constructor(message: String) : this(message, 20)
+
+    fun withMessage(message: String): ActionBarBuilder {
+        this.message = message
+        return this
+    }
+
+    fun replaceMessage(target: String, replacement: String): ActionBarBuilder {
+        message = message.replace(target, replacement)
+        return this
+    }
+
+    fun withDuration(duration: Long): ActionBarBuilder {
+        this.duration = duration
+        return this
+    }
+
+    fun create(): DMActionBar {
+        return DMActionBar(colorString(message), duration)
+    }
 }
