e27f4c5a9f92f35a9cbdf3d1eb75f612c67a4666
DraimCiDo <danilaorlov4@gmail.com>
2022-02-10 08:49:45 +0200

    [1.1.5.1]

--- DMLIB-Spigot/src/main/kotlin/ru/draimdev/dmlibspigot/Config/ConfigManager.kt
+++ DMLIB-Spigot/src/main/kotlin/ru/draimdev/dmlibspigot/Config/ConfigManager.kt
@@ -1,4 +1,34 @@
 package ru.draimdev.dmlibspigot.Config
 
-class ConfigManager {
+import org.bukkit.configuration.file.FileConfiguration
+import org.bukkit.configuration.file.YamlConfiguration
+import org.bukkit.plugin.java.JavaPlugin
+import java.io.File
+import java.io.IOException
+
+class ConfigManager(private val pl: JavaPlugin, private val fileName: String) {
+    val file: File
+    val config: FileConfiguration
+
+    init {
+        val dataFolder = pl.dataFolder
+        if (!dataFolder.exists()) dataFolder.mkdir()         // Если не находит папку - он её создаст
+        this.file = File(dataFolder, fileName)        // Обьяснение что file может находиться в папке и у него есть имя
+        if (!file.exists()) {        // Если не находит файл
+            try {
+                file.createNewFile()                // Попытка создания файла
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+        }
+        this.config = YamlConfiguration.loadConfiguration(file)        // Подхват что конфиг - является этим файлом
+    }
+
+    fun save() {    // Функция на сохранение файла
+        config.save(file)
+    }
+
+    private fun pathNotFound(path: String) { // Если не нашло путь - отпишет что ты еблан
+        pl.logger.warning("Путь $path не был найден в $fileName")
+    }
 }
