91a7368798e1c2eb099abc400bdc97012cbb40c8
DraimCiDo <danilaorlov4@gmail.com>
2022-02-10 17:43:39 +0200

    [1.1.70]

--- DMLIB-Spigot/src/main/kotlin/ru/draimdev/dmlibspigot/Util/HologramUtils.kt
+++ DMLIB-Spigot/src/main/kotlin/ru/draimdev/dmlibspigot/Util/HologramUtils.kt
@@ -1,4 +1,28 @@
 package ru.draimdev.dmlibspigot.Util
 
-class HologramUtils {
+import com.gmail.filoghost.holographicdisplays.api.Hologram
+import com.gmail.filoghost.holographicdisplays.api.HologramsAPI
+import org.bukkit.Bukkit
+import org.bukkit.ChatColor
+import org.bukkit.Location
+import ru.draimdev.dmlibspigot.Config.configuration.Settings
+import ru.draimdev.dmlibspigot.DMLIBSpigot
+
+fun createTemporaryHologram(location: Location, text: List<String>, ticks: Int) {
+    if (!Settings.HOLOGRAMS) {
+        Bukkit.getConsoleSender()
+            .sendMessage(ChatColor.RED.toString() + "DMLIB: Пытался создать Hologram, отображаемую при отключении в конфигурации!")
+        return
+    }
+    val hologram = HologramsAPI.createHologram(DMLIBSpigot.instance, location)
+    for (line in text) {
+        hologram!!.appendTextLine(line)
+    }
+    Bukkit.getScheduler().runTaskLaterAsynchronously(DMLIBSpigot.instance,
+        Runnable {
+            if (hologram != null && !hologram.isDeleted) {
+                hologram.delete()
+            }
+        }, ticks.toLong()
+    )
 }
