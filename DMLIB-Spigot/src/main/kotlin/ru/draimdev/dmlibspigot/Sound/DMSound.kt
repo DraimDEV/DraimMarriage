c31329d861cfa21d40fe7c675b7e74485303df3f
DraimCiDo <danilaorlov4@gmail.com>
2022-02-10 14:30:41 +0200

    [1.1.24]

--- DMLIB-Spigot/src/main/kotlin/ru/draimdev/dmlibspigot/Sound/DMSound.kt
+++ DMLIB-Spigot/src/main/kotlin/ru/draimdev/dmlibspigot/Sound/DMSound.kt
@@ -1,4 +1,12 @@
 package ru.draimdev.dmlibspigot.Sound
 
-class DMSound {
+import org.bukkit.Bukkit
+import org.bukkit.Sound
+import org.bukkit.entity.Player
+
+class DMSound(val type: Sound, val volume: Float, val pitch: Float) {
+
+    fun play(player: Player) = player.playSound(player.location, type, volume, pitch)
+
+    fun broadcast() = Bukkit.getOnlinePlayers().forEach { play(it) }
 }
