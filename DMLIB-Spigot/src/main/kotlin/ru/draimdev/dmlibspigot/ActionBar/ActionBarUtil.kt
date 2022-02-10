eeec048b829c28f3ed1c17816f42d5deb98f55ba
DraimCiDo <danilaorlov4@gmail.com>
2022-02-10 14:41:53 +0200

    [1.1.28]

--- DMLIB-Spigot/src/main/kotlin/ru/draimdev/dmlibspigot/ActionBar/ActionBarUtil.kt
+++ DMLIB-Spigot/src/main/kotlin/ru/draimdev/dmlibspigot/ActionBar/ActionBarUtil.kt
@@ -1,4 +1,24 @@
 package ru.draimdev.dmlibspigot.ActionBar
 
-class ActionBarUtil {
+import com.cryptomorin.xseries.messages.ActionBar
+import org.bukkit.entity.Player
+import ru.draimdev.dmlibspigot.DMLIBSpigot
+
+fun sendActionBar(player: Player, actionBar: DMActionBar) {
+    ActionBar.sendActionBar(
+        DMLIBSpigot.instance,
+        player, actionBar.message, actionBar.duration
+    )
+}
+
+fun broadcastActionBar(actionBar: DMActionBar) {
+    ActionBar.sendPlayersActionBar(actionBar.message)
+}
+
+fun clearActionBar(player: Player) {
+    ActionBar.clearActionBar(player)
+}
+
+fun clearAllActionBars() {
+    ActionBar.clearPlayersActionBar()
 }
