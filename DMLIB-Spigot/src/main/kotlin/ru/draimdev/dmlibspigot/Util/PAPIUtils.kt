8228c2a0b9e8676c240a1f307179d60a6ca38115
DraimCiDo <danilaorlov4@gmail.com>
2022-02-10 15:21:54 +0200

    [1.1.68]

--- DMLIB-Spigot/src/main/kotlin/ru/draimdev/dmlibspigot/Util/PAPIUtils.kt
+++ DMLIB-Spigot/src/main/kotlin/ru/draimdev/dmlibspigot/Util/PAPIUtils.kt
@@ -1,4 +1,18 @@
 package ru.draimdev.dmlibspigot.Util
 
-class PAPIUtils {
+import me.clip.placeholderapi.PlaceholderAPI
+import org.bukkit.Bukkit
+import org.bukkit.ChatColor
+import org.bukkit.entity.Player
+import ru.draimdev.dmlibspigot.Config.configuration.Settings
+
+fun setPlaceholders(player: Player, input: String): String {
+    if (Settings.PLACEHOLDERS) {
+        return PlaceholderAPI.setPlaceholders(player, input)
+    }
+    if (Settings.DISABLED_MESSAGES) {
+        Bukkit.getConsoleSender()
+            .sendMessage(ChatColor.RED.toString() + "DMLib: Попытка про парсить PAPI плейсхолдеры, кажется они отключены в конфиге.")
+    }
+    return input
 }
