b5f6bf388ad4fee7b2cac42639b2355646fa6c0e
DraimCiDo <danilaorlov4@gmail.com>
2022-02-10 17:48:52 +0200

    [1.1.76]

--- DMLIB-Spigot/src/main/kotlin/ru/draimdev/dmlibspigot/ScoreBoard/DMScoreBoard.kt
+++ DMLIB-Spigot/src/main/kotlin/ru/draimdev/dmlibspigot/ScoreBoard/DMScoreBoard.kt
@@ -1,4 +1,28 @@
 package ru.draimdev.dmlibspigot.ScoreBoard
 
-class DMScoreBoard {
+import org.bukkit.Bukkit
+import org.bukkit.entity.Player
+import org.bukkit.scoreboard.DisplaySlot
+import org.bukkit.scoreboard.Scoreboard
+
+class DMScoreBoard(val title: String, val lines: MutableList<String>) {
+
+    fun toScoreboard() : Scoreboard {
+        val scoreBoard = Bukkit.getScoreboardManager().newScoreboard
+
+        val objective = scoreBoard.registerNewObjective(title, "", title)
+        objective.displaySlot = DisplaySlot.SIDEBAR
+
+        var slot = 16
+        lines.forEach {
+            objective.getScore(it).score = slot
+            slot--
+        }
+
+        return scoreBoard
+    }
+
+    fun send(player: Player) = sendScoreBoard(player, this)
+
+    fun broadcast() = Bukkit.getOnlinePlayers().forEach { send(it) }
 }
