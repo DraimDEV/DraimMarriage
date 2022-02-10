f8cd763fd84e09d3b7c88b64bea30c83e749ee62
DraimCiDo <danilaorlov4@gmail.com>
2022-02-10 14:56:35 +0200

    [1.1.42]

--- DMLIB-Spigot/src/main/kotlin/ru/draimdev/dmlibspigot/GUI/events/GUIOpenEvent.kt
+++ DMLIB-Spigot/src/main/kotlin/ru/draimdev/dmlibspigot/GUI/events/GUIOpenEvent.kt
@@ -1,4 +1,19 @@
 package ru.draimdev.dmlibspigot.GUI.events
 
-class GUIOpenEvent {
+import org.bukkit.entity.Player
+import org.bukkit.event.Event
+import org.bukkit.event.HandlerList
+import ru.draimdev.dmlibspigot.GUI.GUI
+
+class GUIOpenEvent(val player: Player, val gui: GUI) : Event() {
+    companion object {
+        private val HANDLERS = HandlerList()
+        fun getHandlerList(): HandlerList {
+            return HANDLERS
+        }
+    }
+
+    override fun getHandlers(): HandlerList {
+        return HANDLERS
+    }
 }
