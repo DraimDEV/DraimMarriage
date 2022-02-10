20caac18709a605efdd7aabd404f52d8f0d3b328
DraimCiDo <danilaorlov4@gmail.com>
2022-02-10 14:56:15 +0200

    [1.1.41]

--- DMLIB-Spigot/src/main/kotlin/ru/draimdev/dmlibspigot/GUI/GUI.kt
+++ DMLIB-Spigot/src/main/kotlin/ru/draimdev/dmlibspigot/GUI/GUI.kt
@@ -1,4 +1,10 @@
 package ru.draimdev.dmlibspigot.GUI
 
-class GUI {
+import org.bukkit.entity.Player
+import org.bukkit.inventory.Inventory
+import ru.draimdev.dmlibspigot.DMLIBSpigot
+
+class GUI(val inventory: Inventory, val items: HashMap<Int, ClickRunnable>, val onClose: GUI? = null) {
+
+    fun open(player: Player) = DMLIBSpigot.guiManager.open(player, this)
 }
