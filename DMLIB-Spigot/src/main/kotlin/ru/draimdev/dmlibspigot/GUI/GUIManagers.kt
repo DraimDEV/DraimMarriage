abf8568858567e8adb9f193326d95450019fbe05
DraimCiDo <danilaorlov4@gmail.com>
2022-02-10 15:07:53 +0200

    [1.1.52]

--- DMLIB-Spigot/src/main/kotlin/ru/draimdev/dmlibspigot/GUI/GUIManagers.kt
+++ DMLIB-Spigot/src/main/kotlin/ru/draimdev/dmlibspigot/GUI/GUIManagers.kt
@@ -1,4 +1,28 @@
 package ru.draimdev.dmlibspigot.GUI
 
-class GUIManagers {
+import org.bukkit.Bukkit
+import org.bukkit.entity.Player
+import ru.draimdev.dmlibspigot.GUI.events.GUIOpenEvent
+import ru.draimdev.dmlibspigot.Player.isPlayer
+import java.util.*
+
+class GUIManager {
+    val guis = HashMap<UUID, GUI>()
+
+    fun open(player: Player, gui: GUI) {
+        player.openInventory(gui.inventory)
+        guis[player.uniqueId] = gui
+        Bukkit.getPluginManager().callEvent(GUIOpenEvent(player, gui))
+    }
+
+    fun closeAll() {
+        guis.forEach { (playerId) ->
+            run {
+                if (isPlayer(playerId)) {
+                    Bukkit.getPlayer(playerId)?.closeInventory()
+                }
+                guis.remove(playerId)
+            }
+        }
+    }
 }
