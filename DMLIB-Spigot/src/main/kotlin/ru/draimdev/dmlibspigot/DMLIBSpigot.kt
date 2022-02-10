9761559d6c2922105ac352507d79f1bbd596a563
DraimCiDo <danilaorlov4@gmail.com>
2022-02-10 15:11:50 +0200

    [1.1.56]

--- DMLIB-Spigot/src/main/kotlin/ru/draimdev/dmlibspigot/DMLIBSpigot.kt
+++ DMLIB-Spigot/src/main/kotlin/ru/draimdev/dmlibspigot/DMLIBSpigot.kt
@@ -5,6 +5,8 @@ import ru.draimdev.dmlibspigot.DataBase.DataSource
 import net.kyori.adventure.platform.bukkit.BukkitAudiences
 import net.milkbowl.vault.economy.Economy
 import ru.draimdev.dmlibspigot.Config.ConfigManager
+import ru.draimdev.dmlibspigot.Config.configuration.Settings
+import ru.draimdev.dmlibspigot.Events.StartupEvent
 import ru.draimdev.dmlibspigot.GUI.GUIManagers
 import ru.draimdev.dmlibspigot.Player.PlayerQuery
 
@@ -27,5 +29,12 @@ class DMLIBSpigot : JavaPlugin() {
     }
 
     override fun onEnable() {
+        instance = this
+        server.pluginManager.callEvent(StartupEvent(this))
+        setupAdventure()
+    }
+
+    private fun setupAdventure() {
+        bukkitAudiences = BukkitAudiences.create(this)
     }
 }
