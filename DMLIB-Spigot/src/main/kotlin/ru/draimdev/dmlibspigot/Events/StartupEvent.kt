de70d449524b3e3ba14288a6245056109c2b93b0
DraimCiDo <danilaorlov4@gmail.com>
2022-02-10 15:10:59 +0200

    [1.1.55]

--- DMLIB-Spigot/src/main/kotlin/ru/draimdev/dmlibspigot/Events/StartupEvent.kt
+++ DMLIB-Spigot/src/main/kotlin/ru/draimdev/dmlibspigot/Events/StartupEvent.kt
@@ -1,4 +1,18 @@
 package ru.draimdev.dmlibspigot.Events
 
-class StartupEvent {
+import org.bukkit.event.Event
+import org.bukkit.event.HandlerList
+import ru.draimdev.dmlibspigot.DMLIBSpigot
+
+class StartupEvent(val plugin: DMLIBSpigot) : Event() {
+    companion object {
+        private val HANDLERS = HandlerList()
+        fun getHandlerList(): HandlerList {
+            return HANDLERS
+        }
+    }
+
+    override fun getHandlers(): HandlerList {
+        return HANDLERS
+    }
 }
