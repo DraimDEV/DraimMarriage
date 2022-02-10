2e1febd03e99af91b4c62e2c969c78b5eb7ff1ed
DraimCiDo <danilaorlov4@gmail.com>
2022-02-10 17:47:57 +0200

    [1.1.74]

--- DMLIB-Spigot/src/main/kotlin/ru/draimdev/dmlibspigot/ScoreBoard/ScoreBoardBuilder.kt
+++ DMLIB-Spigot/src/main/kotlin/ru/draimdev/dmlibspigot/ScoreBoard/ScoreBoardBuilder.kt
@@ -1,4 +1,43 @@
 package ru.draimdev.dmlibspigot.ScoreBoard
 
-class ScoreBoardBuilder {
+import ru.draimdev.dmlibspigot.Util.colorString
+
+
+class ScoreBoardBuilder(var title: String, var lines: MutableList<String>) {
+
+    constructor() : this("", ArrayList())
+
+    constructor(title: String) : this(title, ArrayList())
+
+    constructor(title: String, vararg lines: String) : this(title, lines.toMutableList())
+
+    constructor(scoreboard: DMScoreBoard) : this(scoreboard.title, scoreboard.lines)
+
+    fun withTitle(title: String): ScoreBoardBuilder {
+        this.title = title
+        return this
+    }
+
+    fun withLines(lines: MutableList<String>): ScoreBoardBuilder {
+        this.lines = lines
+        return this
+    }
+
+    fun withLines(vararg lines: String): ScoreBoardBuilder {
+        this.lines = lines.toMutableList()
+        return this
+    }
+
+    fun replaceLines(target: String, input: String): ScoreBoardBuilder {
+        val replacedLines: MutableList<String> = ArrayList()
+        lines.forEach { replacedLines.add(it.replace(target, input)) }
+        lines = replacedLines
+        return this
+    }
+
+    fun create(): DMScoreBoard {
+        val coloredLines: MutableList<String> = java.util.ArrayList()
+        lines.forEach { coloredLines.add(colorString(it)) }
+        return DMScoreBoard(colorString(title), coloredLines)
+    }
 }
