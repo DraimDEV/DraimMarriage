f1d5dbbca850b4f78dc8b1deca7365893330f602
DraimCiDo <danilaorlov4@gmail.com>
2022-02-10 10:23:03 +0200

    [1.1.13] Пропись Type в ConfigManager.kt

--- DMLIB-Spigot/src/main/kotlin/ru/draimdev/dmlibspigot/Config/ConfigManager.kt
+++ DMLIB-Spigot/src/main/kotlin/ru/draimdev/dmlibspigot/Config/ConfigManager.kt
@@ -1,10 +1,23 @@
 package ru.draimdev.dmlibspigot.Config
 
+import com.cryptomorin.xseries.SkullUtils
+import com.cryptomorin.xseries.XEnchantment
+import com.cryptomorin.xseries.XMaterial
+import com.cryptomorin.xseries.XSound
+import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.minimessage.MiniMessage
 import org.bukkit.ChatColor
+import org.bukkit.Material
+import org.bukkit.Sound
 import org.bukkit.configuration.file.FileConfiguration
 import org.bukkit.configuration.file.YamlConfiguration
+import org.bukkit.enchantments.Enchantment
+import org.bukkit.entity.EntityType
+import org.bukkit.inventory.ItemStack
 import org.bukkit.plugin.java.JavaPlugin
+import ru.draimdev.dmlibspigot.Item.ItemBuilder
 import ru.draimdev.dmlibspigot.Util.colorString
+import ru.draimdev.dmlibspigot.Util.isInt
 import java.io.File
 import java.io.IOException
 
@@ -38,6 +51,8 @@ class ConfigManager(private val pl: JavaPlugin, private val fileName: String) {
         pl.logger.warning("$name не является $type в файла $fileName ($path)")
     }
 
+
+    // Список Type
     fun getString(path: String, def: String = "${ChatColor.RED}$path"): String {
         return if (config.isSet(path)) {
             colorString(config.getString(path, def)!!)
@@ -46,5 +61,191 @@ class ConfigManager(private val pl: JavaPlugin, private val fileName: String) {
             def
         }
     }
+
+    fun getSound(path: String, def: Sound = Sound.ENTITY_VILLAGER_NO): Sound {
+        if (config.isSet(path)) {
+            val soundName = getString(path)
+            val xSoundOptional = XSound.matchXSound(soundName)
+            if (xSoundOptional.isPresent) {
+                return xSoundOptional.get().parseSound()!!
+            }
+            isNot("Звук", soundName, path)
+            return def
+        }
+        pathNotFound(path)
+        return def
+    }
+
+    fun getInt(path: String, def: Int = -1): Int {
+        if (config.isSet(path)) {
+            if (config.isInt(path)) {
+                return config.getInt(path, def)
+        }
+            isNot("Ин-ТЕГ", getString(path), path)
+            return def
+        }
+        pathNotFound(path)
+        return def
+    }
+
+    fun getDouble(path: String, def: Double = -1.0): Double {
+        if(config.isSet(path)) {
+            if (config.isDouble(path)) {
+                return config.getDouble(path, def)
+            }
+            isNot("Двойной", getString(path), path)
+        }
+        pathNotFound(path)
+        return def
+    }
+
+    fun getLong(path: String, def: Int = -1) = getInt(path, def).toLong()
+
+    fun getFloat(path: String, def: Double = -1.0) = getDouble(path, def).toFloat()
+
+    fun getBoolean(path: String, def: Boolean = false): Boolean {
+        if (config.isSet(path)) {
+            if (config.isBoolean(path)) {
+                return config.getBoolean(path, def)
+            }
+            isNot("Boolean", getString(path), path)
+            return def
+        }
+        pathNotFound(path)
+        return def
+    }
+
+    fun getStringList(path: String): List<String> {
+        if (config.isSet(path)) {
+            if (config.isList(path)) {
+                val stringList = config.getStringList(path)
+                val coloredStringList: MutableList<String> = ArrayList()
+                for (string in stringList) {
+                    coloredStringList.add(colorString(string))
+                }
+                return coloredStringList
+            }
+            isNot("StringList", "Value", path)
+            return ArrayList()
+        }
+        pathNotFound(path)
+        return ArrayList()
+    }
+
+    fun getEntityType(path: String): EntityType {
+        if (config.isSet(path)) {
+            val entityName = getString(path)
+            for (entity in EntityType.values()) {
+                if (entity.toString().equals(entityName, ignoreCase = true)) {
+                    return entity
+                }
+            }
+            isNot("EntityType", entityName, path)
+            return EntityType.PIG
+        }
+        pathNotFound(path)
+        return EntityType.PIG
+    }
+
+    fun getXMaterial(path: String): XMaterial {
+        if (config.isSet(path)) {
+            val materialName = getString(path)
+            val xMaterialOptional = XMaterial.matchXMaterial(materialName)
+            if (xMaterialOptional.isPresent && xMaterialOptional.get().parseMaterial() != null) {
+                return xMaterialOptional.get()
+            }
+            isNot("Material", materialName, path)
+            return XMaterial.STONE
+        }
+        pathNotFound(path)
+        return XMaterial.STONE
+    }
+
+    fun getMaterial(path: String): Material {
+        if (config.isSet(path)) {
+            val materialName = getString(path)
+            val xMaterialOption = XMaterial.matchXMaterial(materialName)
+            if (xMaterialOption.isPresent && xMaterialOption.get().parseMaterial() != null) {
+                return xMaterialOption.get().parseItem()!!.type
+            }
+            isNot("Material", materialName, path)
+            return Material.STONE
+        }
+        pathNotFound(path)
+        return Material.STONE
+    }
+
+    fun getEnchantments(path: String): HashMap<Enchantment, Int> {
+        val enchs = HashMap<Enchantment, Int>()
+        val enchantments = getStringList(path)
+        for (enchantment in enchantments) {
+            val enchNameAndLevel = enchantment.split(":").toTypedArray()
+            if (enchNameAndLevel.size == 2) {
+                val xEnchantmentOptional = XEnchantment.matchXEnchantment(enchNameAndLevel[0])
+                if (xEnchantmentOptional.isPresent) {
+                    if (isInt(enchNameAndLevel[1])) {
+                        enchs[xEnchantmentOptional.get().parseEnchantment()!!] = enchNameAndLevel[1].toInt()
+                    } else {
+                        isNot("Integer", enchNameAndLevel[1], path)
+                    }
+                } else {
+                    isNot("Enchantment", enchNameAndLevel[0], path)
+                }
+            } else {
+                isNot("Valid Enchantment Format", enchNameAndLevel.contentToString(), path)
+            }
+        }
+        return enchs
+    }
+
+    fun setEnchantments(path: String, enchants: Map<Enchantment, Int>) {
+        val enchs: MutableList<String> = ArrayList()
+        for ((key, value) in enchants) {
+            enchs.add("${key.key}:$value")
+        }
+        config[path] = enchs
+    }
+
+    fun getItemStack(path: String): ItemStack {
+        if (config.isSet(path)) {
+            val itemStack = config.getItemStack(path)
+            if (itemStack == null) {
+                isNot("ItemStack", "?", path)
+                return ItemStack(Material.STONE)
+            }
+            return itemStack
+        }
+        pathNotFound(path)
+        return ItemStack(Material.STONE)
+    }
+
+    fun getHead(path: String): ItemStack {
+        if (config.isSet(path)) {
+            val owner = getString("$path.head-owner")
+            var name = Component.text("Head")
+            var amount = 1
+            val lore: MutableList<Component> = ArrayList()
+            if (config.isSet("$path.amount")) {
+                amount = getInt("$path.amount")
+            }
+            if (config.isSet("$path.name")) {
+                name = Component.text(getString("$path.name"))
+            }
+            if (config.isSet("$path.lore")) {
+                getStringList("$path.lore").forEach { lore.add(Component.text(it)) }
+            }
+            val head: ItemStack = ItemBuilder() // TODO: Мне кажется чего-то не хватает
+            if (config.isSet("$path.enchantments")) {
+                head.addEnchantments(getEnchantments("$path.enchantments"))
+            }
+            return head
+        }
+        pathNotFound(path)
+        return ItemStack(Material.STONE)
+    }
+
+
+
+
 
 }
