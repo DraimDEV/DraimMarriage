acc3719d426cebd1e3685fbfaf464dccbb708f68
DraimCiDo <danilaorlov4@gmail.com>
2022-02-10 15:04:29 +0200

    [1.1.48]

--- DMLIB-Spigot/src/main/kotlin/ru/draimdev/dmlibspigot/Config/configuration/Settings.kt
+++ DMLIB-Spigot/src/main/kotlin/ru/draimdev/dmlibspigot/Config/configuration/Settings.kt
@@ -1,4 +1,23 @@
 package ru.draimdev.dmlibspigot.Config.configuration
 
-class Settings {
+import ru.draimdev.dmlibspigot.Config.ConfigManager
+
+object Settings {
+    private const val PATH = "settings"
+
+    var SOUNDS = true
+    var TELEPORT_DELAY = 7
+    var VAULT = false
+    var HOLOGRAMS = false
+    var PLACEHOLDERS = false
+    var DISABLED_MESSAGES = true
+
+    fun load(config: ConfigManager) {
+        SOUNDS = config.getBoolean("$PATH.sounds", SOUNDS)
+        TELEPORT_DELAY = config.getInt("$PATH.teleport-delay", TELEPORT_DELAY)
+        VAULT = config.getBoolean("$PATH.vault", VAULT)
+        HOLOGRAMS = config.getBoolean("$PATH.holograms", HOLOGRAMS)
+        PLACEHOLDERS = config.getBoolean("$PATH.placeholders", PLACEHOLDERS)
+        DISABLED_MESSAGES = config.getBoolean("$PATH.disabled-messages", DISABLED_MESSAGES)
+    }
 }
