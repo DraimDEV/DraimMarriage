b49ba3ac52de5d2198ee39f23bc1aa40ae53ccb7
DraimCiDo <danilaorlov4@gmail.com>
2022-02-10 14:52:23 +0200

    [1.1.34]

--- DMLIB-Spigot/src/main/kotlin/ru/draimdev/dmlibspigot/Player/PlayerQuery.kt
+++ DMLIB-Spigot/src/main/kotlin/ru/draimdev/dmlibspigot/Player/PlayerQuery.kt
@@ -1,4 +1,34 @@
 package ru.draimdev.dmlibspigot.Player
 
-class PlayerQuery {
+import org.bukkit.plugin.java.JavaPlugin
+import ru.draimdev.dmlibspigot.DataBase.DataSource
+import java.util.*
+
+private const val TABLE_NAMES = "dmlib_playernames"
+
+class PlayerQuery(val dataSource: DataSource, val plugin: JavaPlugin) {
+
+    val playerNames = HashMap<UUID, String>()
+
+    init {
+        dataSource.updateStatement(
+            "CREATE TABLE IF NOT EXISTS $TABLE_NAMES (" +
+                    "uuid char(36) NOT NULL PRIMARY KEY," +
+                    "name varchar(16) NOT NULL);")
+        loadPlayerNames()
+    }
+
+    private fun loadPlayerNames() {
+        val statement = dataSource.getConnection().prepareStatement("SELECT * FROM $TABLE_NAMES")
+        val results = statement.executeQuery()
+        while (results.next()){
+            playerNames[UUID.fromString(results.getString("uuid"))] = results.getString("name")
+        }
+        dataSource.close(statement)
+    }
+
+    fun savePlayerName(uuid: UUID, name: String) {
+        dataSource.updateStatement("REPLACE INTO $TABLE_NAMES VALUES ('$uuid','$name')")
+        playerNames[uuid] = name
+    }
 }
